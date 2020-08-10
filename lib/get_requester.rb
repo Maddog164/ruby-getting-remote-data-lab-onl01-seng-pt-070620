07bbb70263ff78491afbc4e2c965c7a90264b1a4
Denise Henderson <denise@henderson3.us>
2020-08-10 20:51:43 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,6 +4,17 @@ require 'json'
 
 class GetRequester
 
+URL = 
+
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  endpoints
+
+  def parse_json
+    
+
 
 
 
